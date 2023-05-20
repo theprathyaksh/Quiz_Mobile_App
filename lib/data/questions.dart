d8a448170fef3ee21c4114de90022899f2519535
theprathyaksh <iamprathyaksh@gmail.com>
2023-05-20 22:23:46 +0530

    first commit

---
+++ lib/data/questions.dart
@@ -0,0 +1,31 @@
+import 'package:second_app/models/quiz_questions.dart';
+
+const questions = [
+  QuizQuestion(
+    'Whos is the Chief Minister of Andhra Pradesh?',
+    [
+      'Jagan Mohan Reddy',
+      'KCR',
+      'Sunil Narine',
+      'Manmohan Singh',
+    ],
+  ),
+  QuizQuestion(
+    'Who is the captain of Gujarat Titans?',
+    [
+      'Hardik',
+      'Dhoni',
+      'Jadeja',
+      'Virat',
+    ],
+  ),
+  QuizQuestion(
+    'Who is the worlds richest person in 2030?',
+    [
+      'Prathyaksh Malligari',
+      'Jeff Bezos',
+      'Elon Musk',
+      'Mukesh Ambani',
+    ],
+  ),
+];
---
+++ lib/quiz.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+import 'package:second_app/start_screen.dart';
+import 'package:second_app/questions_screen.dart';
+import 'package:second_app/data/questions.dart';
+import 'package:second_app/results_screen.dart';
+
+class Quiz extends StatefulWidget {
+  const Quiz({super.key});
+
+  @override
+  State<Quiz> createState() {
+    return _QuizState();
+  }
+}
+
+class _QuizState extends State<Quiz> {
+  List<String> selectedAnswers = [];
+  var activeScreen = 'start-screen';
+
+  // @override
+  // void initState() {
+  //   activeScreen = 'questions-screen';
+  //   super.initState();
+  // }
+
+  void switchScreen() {
+    setState(() {
+      activeScreen = 'questions-screen';
+    });
+  }
+
+  void chooseAnswer(String answer) {
+    selectedAnswers.add(answer);
+
+    if (selectedAnswers.length == questions.length) {
+      setState(() {
+        activeScreen = 'results-screen';
+      });
+    }
+  }
+
+  @override
+  Widget build(context) {
+    var screenWidget = activeScreen == 'start-screen'
+        ? StartScreen(switchScreen)
+        : QuestionsScreen(
+            onSelectAnswer: chooseAnswer,
+          );
+
+    if (activeScreen == 'results-screen') {
+      screenWidget = ResultsScreen(
+        chosenAnswers: selectedAnswers,
+      );
+    }
+    return MaterialApp(
+      home: Scaffold(
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Color.fromARGB(255, 78, 13, 151),
+                Color.fromARGB(255, 107, 15, 168),
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+          ),
+          child: screenWidget,
+        ),
+      ),
+    );
+  }
+}
